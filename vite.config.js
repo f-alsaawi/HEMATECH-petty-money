c6a6577024cc8815e28be4b2a3fa4eace16482b9
abb1418-cpu <abb1418@hotmail.com>
2026-06-21 18:04:27 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// base: './' يجعل المسارات نسبية لتعمل على GitHub Pages تحت أي اسم مستودع
-export default defineConfig({
-  plugins: [react()],
-  base: './'
-});
